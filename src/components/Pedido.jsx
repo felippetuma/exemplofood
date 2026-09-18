1ffd5743b11c6d14269299b35d7b961732a34bce
Felippe Tuma <felippetuma@outlook.com>
2026-09-18 09:16:07 -0300

    feat: funções do pedido

--- src/components/Pedido.jsx
+++ src/components/Pedido.jsx
@@ -10,6 +10,56 @@ const cardapio = [
 ]
 
 const Pedido = () => {
+
+  // HOOK - userState = Manipul o estados da variavel
+  // Estudos para gerenciar a lista de items do cardapio
+
+  const [items, setItems] = useState(cardapio);
+  const [status, setStatus] = useState(" ");
+  const [enviar, setEnviar] = useState(false);
+
+  // Valor fixo adicionado ao total quando existir items no carrino
+
+  const taxaEntrega = 5.00;
+
+  // Função que altera a quantiade do pedido
+
+  const AlterarQuantidade = (id, valor) => {
+    setItems(alt => 
+      // map , filter, reduce
+      // MAP: CRIA O ARRAY R PERCORRE OS ITENS SEM MODIFICAR O ORIGINAL (IMUTABILIDADE)
+      alt.map(items => 
+        // TERNARIO: verifica se o item de uma interação atual é o que pode ser alterado
+        // SPREAD (..items): manter os valores antigos e adiciona novos
+        // MATH.MAX: objeto que garante a quantidade nunca seja menor que 0
+        items.id === id ? [...items.quantidade, Math.max(0, items.quantidade + valor)]: items
+      )
+
+    )
+  }
+
+  // FILTER: Seleciona apenas os produtos disponiveis no carrino
+  const produtoDisponiveis = items.filter(items => items.disponivel);
+  const carrinho =  items.filter(items => items.quantidade > 0);
+
+  // REDUCE: Calcula a soma dos itens (preco + quantidade) > 0 adiciona a taxa de entrega
+  const subTotal = carrinho.reduce((act, items) => ac + items.preco * items.quantidade);
+  const total = subTotal > 0  ? subTotal + taxaEntrega: 0;
+  
+  // SIMULAÇÃO DO CICLO DE VIDA DE ENTREGA USANDO TEMPORIZADOR ASSINCRONO
+  const ConfirmarPdeido = () => {
+      setEnviar(true);
+      setStatus("Restaurante Confirmou pagamento, preparando pedido");
+      setTimeout(() => {
+        setStatus("Seu pedido saiu para entrega");
+        setEnviar(false);
+      }, 5000);
+      setTimeout(() => {
+        setStatus("Seu pedido foi entregue com sucesso");
+        setEnviar(false);
+      }, 10000);
+  }
+
   return (
     <>
       
