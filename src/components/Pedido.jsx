cf0d91d693aab67c6f3f7673bd78feb5a0ab9334
Felippe Tuma <felippetuma@outlook.com>
2026-09-18 08:44:26 -0300

    feat: adiciona banco de dados no projeto

--- src/components/Pedido.jsx
+++ src/components/Pedido.jsx
@@ -1,3 +1,13 @@
+import { useState } from "react";
+
+//Array  de objetos contendo o estado inicia do cardapio
+
+const cardapio = [
+  {id: 1, nome : "Combo-1", preco: 25.00, disponivel: true, quantidade: 0},
+  {id: 2, nome : "Combo-2", preco: 35.00, disponivel: true, quantidade: 0},
+  {id: 3, nome : "Combo-3", preco: 45.00, disponivel: false, quantidade: 0},
+  {id: 4, nome : "Combo-4", preco: 55.00, disponivel: true, quantidade: 0},
+]
 
 const Pedido = () => {
   return (
